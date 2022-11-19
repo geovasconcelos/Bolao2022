333d0c482d9ec2481f129698c03dbc1341cd6cd4
geovasconcelos <66443273+geovasconcelos@users.noreply.github.com>
2022-11-18 23:14:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,7 @@ df.sort_values(by=['Pontos','Participantes'], ascending=True, inplace=True)
 #####################################################################################
 
 tab = pd.read_excel('https://github.com/geovasconcelos/Bolao2022/blob/main/Copa2022.xlsm?raw=true', engine="openpyxl", sheet_name='Placar')
-tab.drop(['Grupo', 'Local','Unnamed: 10', 'Unnamed: 11', 'Unnamed: 12', 'Unnamed: 13', 'Unnamed: 14', 'Unnamed: 15', 'Unnamed: 16', 'Unnamed: 17',
-       'Unnamed: 18', 'Unnamed: 19', 'Unnamed: 20', 'Unnamed: 21', 'Unnamed: 22', 'Unnamed: 23','Unnamed: 24'], axis=1, inplace=True)
+tab.drop(['Grupo', 'Local','Unnamed: 10', 'Unnamed: 11', 'Unnamed: 12', 'Unnamed: 13', 'Unnamed: 14', 'Unnamed: 15'], axis=1, inplace=True)
 tab.columns=['Jogo', "Dia","Hora", "Equipe1", "Gol1", "X", "Gol2", "Equipe2"]
 tab.dropna(subset=['Equipe2'], inplace=True)
 tab['Dia'] = pd.to_datetime(tab['Dia'], format="%d/%m/%Y")
